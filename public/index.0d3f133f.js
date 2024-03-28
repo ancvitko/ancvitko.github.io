4bf554918444b22893e00fcc76bb53d76a27ce75
Antonio Cvitkovic <125733906+ancvitko@users.noreply.github.com>
2024-03-28 18:03:19 +0100

    Removed smoothscroll and added locomotive scroll

---
+++ public/index.0d3f133f.js
@@ -0,0 +1,2 @@
+const e=new IntersectionObserver(e=>{e.forEach(e=>{console.log(e),e.isIntersecting?e.target.classList.add("show"):e.target.classList.remove("show")})});document.querySelectorAll(".hidden").forEach(s=>{e.observe(s)});
+//# sourceMappingURL=index.0d3f133f.js.map
